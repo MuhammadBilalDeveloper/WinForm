671767b676d4f4eabf03ad101ebc09ffe5d6fc95
Muhammad Bilal <yasir007.in@gmail.com>
2021-11-16 01:05:01 +0500

    Added TestProject

---
+++ WinFormUI.UnitTests/CustomerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data;
+using WinFormDAL;
+using WinFormEntity;
+
+namespace WinFormUI.UnitTests
+{
+    [TestClass]
+    public class CustomerTests
+    {
+
+        [TestMethod]
+        public void GetCustomer_Any_ReturnsCustomerList()
+        {
+            // Arrange
+            ICustomerRepositry _customerRepositry = new CustomerRepositry();
+            Customer customerobj = new Customer();
+            DataTable dataTable = new DataTable();
+            // Act
+            dataTable = _customerRepositry.GetCustomer(customerobj);
+            // Assert
+            Assert.IsTrue(dataTable.Rows.Count > 0);
+
+        }
+}
+}
---
+++ WinFormUI.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WinFormUI.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinFormUI.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1796d7c2-c2d5-45ca-95ac-b3876aaeff49")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
